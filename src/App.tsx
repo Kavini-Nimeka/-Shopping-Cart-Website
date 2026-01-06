be5e2b32ee793dd87c8e0b18cebf940e81a0f185
Kavini-Nimeka <nimekakavii514@gmail.com>
2026-01-06 09:50:10 +0530

    Initial commit for new

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import './App.css'
+import Header from './component/Header'
+import Footer from './component/Footer'
+import Cart from './component/Cart'
+import ProductList from './component/ProductList'
+import { useState } from 'react'
 
 function App() {
+  const [viewCart, setViewCart] = useState<boolean>(false)
 
-
-  return (
+  const pageContent = viewCart ? <Cart/>:<Cart/>
+  const content = (
     <>
-      <div>
-      </div>
+    <Header viewCart = {viewCart} setViewCart = {setViewCart}/>
+    {pageContent}
+    <Footer viewCart = {viewCart}/>
     </>
   )
+
+  return content
 }
 
 export default App
